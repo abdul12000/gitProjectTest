f0d0d8feed9f0a21109568debd65729a36c5414e
lateef abdulsalam <abdulsalam12000@hotmail.com>
2023-02-10 18:21:45 +0000

    adding Main3

---
+++ src/main/java/org/example/Main3.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world again and again!");
+        System.out.println("Hello world again and again!");
+    }
+}
